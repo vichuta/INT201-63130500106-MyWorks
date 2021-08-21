ed276871fb0211b749c477aeb2873d220bcf178a
vichuta <vichuta.nice@mail.kmutt.ac.th>
2021-08-21 16:46:16 +0700

    update test.js

--- 01_myTest/test.js
+++ 01_myTest/test.js
@@ -5,3 +5,20 @@ const z = "con";
 console.log(x);
 console.log(y);
 console.log(z);
+
+
+let a = 1;
+a = 3;
+function A(){
+    let b = 2;
+    console.log(a);
+    console.log(b);
+}
+//ปริ้นท์ไม่ออก
+//console.log(b);
+function B(){
+    console.log(a);
+    //console.log(b);
+}
+A();// 1 2
+B();// 1
